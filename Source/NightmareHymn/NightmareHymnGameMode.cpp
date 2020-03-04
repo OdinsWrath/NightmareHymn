d38164af1742c1b559bed5fc4f55cdf46d73fe40
Rkenfield <Ryanjkenfield@aol.com>
2020-03-04 16:16:32 -0700

    asd

--- Source/NightmareHymn/NightmareHymnGameMode.cpp
+++ Source/NightmareHymn/NightmareHymnGameMode.cpp
@@ -7,7 +7,7 @@
 ANightmareHymnGameMode::ANightmareHymnGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_BaseChar"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
